8ab476fe5642b87b3cc57f9c8a48a96f30b7c6f6
tushardahiya <tushardahiya84@gmail.com>
2021-05-09 23:40:45 +0530

    laid out the movie cards

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Movie from "./components/Movie";
 
 const FEATURED_API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1e3d27f0aefcf69a0745ea2244e98e1e&page=1";
-const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   "https://api.themoviedb.org/3/search/movie?&api_key=1e3d27f0aefcf69a0745ea2244e98e1e&query=";
 
@@ -20,9 +19,9 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <div className="movie-container">
       {movies.length > 0 &&
-        movies.map((movie) => <Movie key={movie.id} data={movie} />)}
+        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,6 +6,10 @@ const Movie = ({ title, poster_path, overview, vote_average }) => {
   return (
     <div className="movie">
       <img src={IMG_API + poster_path} alt={title} />
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span>{vote_average}</span>
+      </div>
     </div>
   );
 };
