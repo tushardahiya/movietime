0568d5c8445011d7fc750a7b555a30d817ce741a
tushardahiya <tushardahiya84@gmail.com>
2021-05-09 23:31:01 +0530

    completed fetching movies from api and storing them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1e3d27f0aefcf69a0745ea2244e98e1e&page=1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=1e3d27f0aefcf69a0745ea2244e98e1e&query=";
 
 function App() {
-  return <div>hello from App.js</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
+
+  return (
+    <div>
+      {movies.length > 0 &&
+        movies.map((movie) => <Movie key={movie.id} data={movie} />)}
+    </div>
+  );
 }
 
 export default App;
+
+//API Key (v3 auth)
+//1e3d27f0aefcf69a0745ea2244e98e1e
+
+// example API request
+// https://api.themoviedb.org/3/movie/550?api_key=1e3d27f0aefcf69a0745ea2244e98e1e
