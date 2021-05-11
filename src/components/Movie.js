4d46f52a23a96ac8e6652e8167ea7db1ab2df8e0
tushardahiya <tushardahiya84@gmail.com>
2021-05-11 14:17:24 +0530

    added fallback image logic + changed the favicon + removed react-image-fallback dependency

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import fallbackimage from "../assets/image_not_found.png";
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 const setVoteClass = (vote) => {
@@ -15,7 +15,10 @@ const setVoteClass = (vote) => {
 const Movie = ({ title, poster_path, overview, vote_average }) => {
   return (
     <div className="movie">
-      <img src={IMG_API + poster_path} alt="404 poster not found" />
+      <img
+        src={poster_path ? IMG_API + poster_path : fallbackimage}
+        alt="404 poster not found"
+      />
       <div className="movie-info">
         <h3>{title}</h3>
         <span className={`tag ${setVoteClass(vote_average)}`}>
