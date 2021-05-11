dee3c8e941935e521b2de92e2012b534f2de44c3
tushardahiya <tushardahiya84@gmail.com>
2021-05-11 12:17:29 +0530

    added search functionality

--- src/App.js
+++ src/App.js
@@ -10,17 +10,25 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
-    fetch(FEATURED_API)
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setMovies(data.results);
       });
+  };
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
   }, []);
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm("");
+    }
   };
 
   const handleOnChange = (e) => {
